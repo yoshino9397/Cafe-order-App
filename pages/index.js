2fe040bd5d15051edc1debf2b7a1f6ae5060fa5a
矢山佳乃 <yayamayoshino@yayamayoshinonoMacBook-Air.local>
2022-04-15 01:09:31 -0700

    first commit

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Cafe Lynx in NY</title>
+        <meta name="description" content="Good coffee makes your day" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      homepage
+    </div>
+  );
+}

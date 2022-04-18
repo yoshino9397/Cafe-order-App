def00ac7814a7bd7c6ecde6ed9942fc7d76b8bbd
矢山佳乃 <yayamayoshino@yayamayoshinonoMacBook-Air.local>
2022-04-17 18:45:11 -0700

    product responsive DONE

---
+++ pages/cart.jsx
@@ -0,0 +1,94 @@
+import styles from "../styles/Cart.module.css";
+import Image from "next/image";
+
+const Cart = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.left}>
+        <table className={styles.table}>
+          <tbody>
+            <tr className={styles.trTitle}>
+              <th>Product</th>
+              <th>Name</th>
+              <th>Extras</th>
+              <th>Price</th>
+              <th>Quantity</th>
+              <th>Total</th>
+            </tr>
+            <tr className={styles.tr}>
+              <td>
+                <div className={styles.imgContainer}>
+                  <Image
+                    src="/img/americano.jpg"
+                    layout="fill"
+                    objectFit="cover"
+                    alt=""
+                  />
+                </div>
+              </td>
+              <td>
+                <span className={styles.name}>Americano</span>
+              </td>
+              <td>
+                <span className={styles.extras}>Extra syrup</span>
+              </td>
+              <td>
+                <span className={styles.price}>$4.70</span>
+              </td>
+              <td>
+                <span className={styles.quantity}>2</span>
+              </td>
+              <td>
+                <span className={styles.total}>$9.40</span>
+              </td>
+            </tr>
+            <tr className={styles.tr}>
+              <td>
+                <div className={styles.imgContainer}>
+                  <Image
+                    src="/img/coffee.jpg"
+                    layout="fill"
+                    objectFit="cover"
+                    alt=""
+                  />
+                </div>
+              </td>
+              <td>
+                <span className={styles.name}>Blend Coffee</span>
+              </td>
+              <td>
+                <span className={styles.extras}>Soy milk</span>
+              </td>
+              <td>
+                <span className={styles.price}>$3.90</span>
+              </td>
+              <td>
+                <span className={styles.quantity}>3</span>
+              </td>
+              <td>
+                <span className={styles.total}>$11.70</span>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div className={styles.right}>
+        <div className={styles.wrapper}>
+          <h2 className={styles.title}>CART TOTAL</h2>
+          <div className={styles.totalText}>
+            <b className={styles.totalTextTitle}>Subtotal:</b>$21.10
+          </div>
+          <div className={styles.totalText}>
+            <b className={styles.totalTextTitle}>Discount:</b>$0.00
+          </div>
+          <div className={styles.totalText}>
+            <b className={styles.totalTextTitle}>Total:</b>$21.10
+          </div>
+          <button className={styles.button}>CHECKOUT NOW!</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;

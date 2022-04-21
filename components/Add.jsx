d41b179548569f79ad18525cebf72b8f417c22db
矢山佳乃 <yayamayoshino@yayamayoshinonoMacBook-Air.local>
2022-04-21 01:56:31 -0700

    new product

---
+++ components/Add.jsx
@@ -0,0 +1,113 @@
+import { useState } from "react";
+import styles from "../styles/Add.module.css";
+import axios from "axios";
+import { useRouter } from "next/router";
+import { MdCancel } from "react-icons/md";
+
+const Add = ({ setClose }) => {
+  const [file, setFile] = useState(null);
+  const [title, setTitle] = useState(null);
+  const [desc, setDesc] = useState(null);
+  const [extra, setExtra] = useState(null);
+  const [prices, setPrices] = useState([]);
+  const [extraOptions, setExtraOptions] = useState([]);
+
+  const handleExtraInput = (e) => {
+    setExtra({ ...extra, [e.target.name]: e.target.value });
+  };
+
+  const handleExtra = (e) => {
+    setExtraOptions((prev) => [...prev, extra]);
+  };
+
+  const changePrice = (e, index) => {
+    const currentPrices = prices;
+    currentPrices[index] = e.target.value;
+    setPrices(currentPrices);
+  };
+
+  const handleCreate = async () => {
+
+
+  }
+  
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <span onClick={() => setClose(true)} className={styles.close}>
+          <MdCancel fontSize="27px" />
+        </span>
+        <h1>Add a new Drink</h1>
+        <div className={styles.item}>
+          <label className={styles.label}>Choose an image</label>
+          <input type="file" onChange={(e) => setFile(e.target.files[0])} />
+        </div>
+        <div className={styles.item}>
+          <label className={styles.label}>Desc</label>
+          <textarea
+            rows={4}
+            type="text"
+            onChange={(e) => setDesc(e.target.value)}
+          />
+        </div>
+        <div className={styles.item}>
+          <label className={styles.label}>Prices</label>
+          <div className={styles.priceContainer}>
+            <input
+              className={`${styles.input} ${styles.inputSm}`}
+              type="number"
+              placeholder="Small"
+              onChange={(e) => changePrice(e, 0)}
+            />
+            <input
+              className={`${styles.input} ${styles.inputSm}`}
+              type="number"
+              placeholder="Medium"
+              onChange={(e) => changePrice(e, 1)}
+            />
+            <input
+              className={`${styles.input} ${styles.inputSm}`}
+              type="number"
+              placeholder="Large"
+              onChange={(e) => changePrice(e, 2)}
+            />
+          </div>
+        </div>
+        <div className={styles.item}>
+          <label className={styles.label}>Extra</label>
+          <div className={styles.extra}>
+            <input
+              className={`${styles.input} ${styles.inputSm}`}
+              type="text"
+              placeholder="Item"
+              name="text"
+              onChange={handleExtraInput}
+            />
+            <input
+              className={`${styles.input} ${styles.inputSm}`}
+              type="number"
+              placeholder="Price"
+              name="price"
+              onChange={handleExtraInput}
+            />
+            <button className={styles.extraButton} onClick={handleExtra}>
+              Add
+            </button>
+          </div>
+          <div className={styles.extraItems}>
+            {extraOptions.map((option) => (
+              <span key={option.text} className={styles.extraItem}>
+                {option.text}
+              </span>
+            ))}
+          </div>
+        </div>
+        <button className={styles.addButton} onClick={handleCreate}>
+          Create
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Add;
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,16 @@
 import axios from "axios";
 import Head from "next/head";
 import Image from "next/image";
+import { useState } from "react";
+import Add from "../components/Add";
+import AddButton from "../components/AddButton";
 import CafeList from "../components/CafeList";
 import Featured from "../components/Featured";
 import Top from "../components/Top";
 import styles from "../styles/Home.module.css";
 
-export default function Home({ cafeList }) {
+export default function Home({ cafeList, admin }) {
+  const [close, setClose] = useState(true);
   return (
     <div className={styles.container}>
       <Head>
@@ -16,16 +20,25 @@ export default function Home({ cafeList }) {
       </Head>
       <Top />
       <Featured />
+      {admin && <AddButton setClose={setClose} />}
       <CafeList cafeList={cafeList} />
+      {!close && <Add setClose={setClose} />}
     </div>
   );
 }
 
-export const getServerSideProps = async () => {
+export const getServerSideProps = async (ctx) => {
+  const myCookie = ctx.req?.cookies || "";
+  let admin = false;
+
+  if (myCookie.token === process.env.TOKEN) {
+    admin = true;
+  }
   const res = await axios.get("http://localhost:3000/api/products");
   return {
     props: {
       cafeList: res.data,
+      admin,
     },
   };
 };
